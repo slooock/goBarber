3ea4f6e8b174f6be84c82646244f5ee980df74d8
Kayque <kayque.aavelar@gmail.com>
2020-09-01 09:17:53 -0300

    image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
-import {Router} from 'express';
+import { Router, request } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated  from '../middlewares/ensureAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response)=>{
   
@@ -22,6 +26,14 @@ usersRouter.post('/', async (request, response)=>{
   } catch(err){
     return response.status(400).json({error:  err.message});
   }
+});
+
+usersRouter.patch('/avatar', 
+ensureAuthenticated, 
+upload.single('avatar'), 
+async (request, response) => {
+  console.log(request.file);
+  return response.json({ ok: true });
 });
 
 export default usersRouter;
