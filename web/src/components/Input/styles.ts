23716081f28c94e13c4512bdd45f048f577b3654
kayque <kayque.avelar@gmail.com>
2021-01-13 12:09:56 -0300

    usabilidade input

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,12 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
 
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
     background: #232129;
     border-radius: 10px;
-    border: 2px solid #232129;
     padding: 16px;
     width: 100%;
+
     color: #666360;
+    border: 2px solid #232129;
 
     display: flex;
     align-items: center;
@@ -16,7 +21,16 @@ export const Container = styled.div`
 
     svg {
         margin-right: 16px;
-      }
+    }
+
+    ${props => props.isFocused && css `
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+    ${props => props.isFilled && css `
+      color: #ff9000;
+    `}
 
    input {
       flex: 1;
