dd3a16af2ca11c0b665b1f1dac2706ca175d340b
kayque <kayque.avelar@gmail.com>
2021-01-14 11:58:42 -0300

    habilitar cors aplicacao

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
+import cors from 'cors';
 
 import './database'
 import uploadConfig from './config/upload';
@@ -9,6 +10,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
