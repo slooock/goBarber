a3b96723960e46796ec07377c4ea387cd2214944
kayque <kayque.avelar@gmail.com>
2021-01-15 17:18:57 -0300

    login pelo contexto

--- web/src/pages/signin/index.tsx
+++ web/src/pages/signin/index.tsx
@@ -4,7 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationErrors';
-import AuthContext from '../../context/AuthContext';
+import {AuthContext} from '../../context/AuthContext';
 
 import logoImg from '../../asseets/logo.svg';
 
@@ -13,13 +13,17 @@ import { Background, Content, Container } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface SignInFormData {
+  email: string,
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const auth = useContext(AuthContext);
-  console.log(auth);
+  const { signIn } = useContext(AuthContext);
 
-  const handleSubmit = useCallback(async (data: Object)=>{
+  const handleSubmit = useCallback(async (data: SignInFormData)=>{
     try{
       formRef.current?.setErrors({});
 
@@ -31,14 +35,16 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false
       });
-
-
+      signIn({
+        email: data.email,
+        password: data.password
+      });
     }catch(err){
       console.log(err);
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }
-  },[]);
+  },[signIn]);
 
   return (
   <>
