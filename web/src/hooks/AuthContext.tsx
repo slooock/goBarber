62ad7686f3aff29ae2d64145324385d27c100a6f
kayque <kayque.avelar@gmail.com>
2021-01-25 15:31:04 -0300

    logout da aplicacao

--- web/src/hooks/AuthContext.tsx
+++ web/src/hooks/AuthContext.tsx
@@ -13,6 +13,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void
 }
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 const AuthProvider: React.FC = ({ children }) => {
@@ -24,6 +25,14 @@ const AuthProvider: React.FC = ({ children }) => {
     }
     return {} as AuthState;
   });
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, [])
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
@@ -35,7 +44,7 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
